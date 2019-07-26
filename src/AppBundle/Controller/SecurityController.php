81e969f10c5a02bd4df050e47308841744599263
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-27 02:57:30 +0300

    Symfony project

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
+}
---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("register", name="user_register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function register(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $passwordHash = $this->get("security.password_encoder")
+                ->encodePassword($user, $user->getPassword());
+
+
+            $user->setPassword($passwordHash);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute("security_login");
+        }
+        return $this->render('users/register.html.twig');
+    }
+}
