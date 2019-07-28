f197b1a71b03df4126cce700ffabeae1630066c1
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-07-28 21:28:38 +0300

    Symfony project

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,17 +2,20 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/login", name="security_login")
-     * @param $name
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function login()
+    public function login(Request $request)
     {
         return $this->render('security/login.html.twig');
     }
