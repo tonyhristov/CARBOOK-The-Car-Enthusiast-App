788b4514ce86c1c3e7185d5d47c82c9fd492ecf2
Antonio <47687247+tonyhristov@users.noreply.github.com>
2019-08-04 01:21:15 +0300

    Symfony project

---
+++ src/AppBundle/Service/Encryption/EncryptionServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace AppBundle\Service\Encryption;
+
+
+interface EncryptionServiceInterface
+{
+    public function hash(string $password);
+
+    public function verify(string $password, string $hash);
+
+}
